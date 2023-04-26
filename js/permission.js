233a5ca85949b69e0716cc2d73767ea67b6a544e
Joao20Victor <joao.super.sayajin.007@gmail.com>
2023-04-26 14:01:23 -0300

    .

--- js/permission.js
+++ js/permission.js
@@ -1,1 +1,51 @@
 let permission = JSON.parse(localStorage.getItem("permission")) || []
+function SetItem(){
+    localStorage.setItem("permission", JSON.stringify(permission))
+}
+function addPermission(){
+    const namePermission = document.querySelector(".inputName")
+    const id = permission.length
+    permission.push({name: namePermission.value, id: id})
+    namePermission.value = ""
+    renderPermissions()
+    closeModal()
+    SetItem()
+}
+
+function renderPermissions(){
+    const RenderPermissions = document.querySelector(".permissoes-ativadas")
+    RenderPermissions.innerHTML = null
+    permission.forEach(function(permission) {
+        const div = document.createElement("div")
+        div.classList.add("permissao-ativada")
+
+        const namePermission = document.createElement("span")
+        namePermission.classList.add("permisao-texto")
+        namePermission.innerText = permission.name
+        div.appendChild(namePermission)
+        RenderPermissions.appendChild(div)
+    })
+
+}
+
+function openModal() {
+    const modal = document.getElementById("pop")
+    modal.style.display = "flex";
+} 
+
+function closeModal() {
+    const modal = document.getElementById("pop")
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("pop")
+  if (event.target == modal) {
+    closeModal();
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    renderPermissions()
+})
